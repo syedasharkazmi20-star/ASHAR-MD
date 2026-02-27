cb3e31d6caf4bf7b19c44cd5ee8514fd3cb8373b
syedasharkazmi20-star <syedasharkazmi20@gmail.com>
2026-02-27 03:11:43 -0800

    Update configuration values for owner and bot

--- config.js
+++ config.js
@@ -12,11 +12,11 @@ module.exports = {
     AUTO_STATUS_REACTS: process.env.AUTO_STATUS_REACTS || "true",
     AUTO_STATUS_EMOJIS: process.env.AUTO_STATUS_EMOJIS || "❤️,💀,🌚,🌟,🔥,❤️‍🩹,🌸,🍁,🍂,🦋,🍥,🍧,🍨,🍫,🍭,🎀,🎐,🎗️,👑,🚩,🇵🇰,🍓,🍇,🧃,🗿,🎋,💸,🧸,🦢,✨,🌾,🌊,⚡,🌏,🕸️,🎀,🪄,🌝,🌜,💫,🤍,🖤,🤎,💜,💙", // Input your custom emojis
     AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-    STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ STATUS VIEWED BY STARK-MD", // Custom message
+    STATUS_REPLY_MSG: process.env.STATUS_REPLY_MSG || "✅️ STATUS VIEWED BY ASHAR-MD", // Custom message
     MODE: process.env.MODE || "public", // Options: private, public, inbox, groups
     TGTOKEN:process.env.TGTOKEN || "7672295852:AAG0SEMHbM1jhkpodxHspJuVT5tiAhXPPpI",
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "923199471258", // Only 1 owner number here, others add to sudo numbers
-    OWNER_NAME: process.env.OWNER_NAME || "𓂃ᷱ᪳𝐀ɭīī 𝐈𝐍𝅦𝐗īī𝐃𝐄^᪲᪲᪲輪", // Custom name
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "923424085673", // Only 1 owner number here, others add to sudo numbers
+    OWNER_NAME: process.env.OWNER_NAME || "Syed Ashar Kazmi", // Custom name
     PACK_AUTHOR: process.env.PACK_AUTHOR || "", // Custom
     PACK_NAME: process.env.PACK_NAME || "",
     PREFIX: process.env.PREFIX || ".",
@@ -26,18 +26,18 @@ module.exports = {
     ANTIBAD: process.env.ANTIBAD || "false",
     BAD_WORDS: process.env.BAD_WORDS || "fuck, pussy, anus, idiot", // Will be deleted if ANTIBAD is true
     ANTI_CALL_MSG: process.env.ANTI_CALL_MSG || "*📞 ᴄαℓℓ ɴσт αℓℓσωє∂ ιɴ тнιѕ ɴᴜмвєʀ уσυ ∂σɴт нανє ᴘєʀмιѕѕισɴ 📵*",
-    AUTO_REACT: process.env.AUTO_REACT || "false",
+    AUTO_REACT: process.env.AUTO_REACT || "true",
     OWNER_REACT: process.env.OWNER_REACT || "false",
-    BOT_NAME: process.env.BOT_NAME || "𓆩ု᪳𝐒𝐓𝐀𝐑𝐊-𝐌𝐃ှ᪳𓆪", // Don't change
-    BOT_PIC: process.env.BOT_PIC || "https://files.catbox.moe/2ka956.jpg", // Don't change
+    BOT_NAME: process.env.BOT_NAME || "ASHAR-MD", // Don't change
+    BOT_PIC: process.env.BOT_PIC || "https://i.postimg.cc/jSHH1cgh/IMG-20260226-WA0014.jpg", // Don't change
     AUTO_AUDIO: process.env.AUTO_AUDIO || "false",
     AUTO_BIO: process.env.AUTO_BIO || "false",
-    AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || "STARK MD ALIVE",
+    AUTO_BIO_QUOTE: process.env.AUTO_BIO_QUOTE || "ASHAR MD ALIVE",
     WELCOME: process.env.WELCOME || "false",
     AUTO_READ_MESSAGES: process.env.AUTO_READ_MESSAGES || "false", // true = bluetick all messages, commands = bluetick commands only
     AUTO_BLOCK: process.env.AUTO_BLOCK || "333,799", // Multiple country codes separated by comma
     PRESENCE: process.env.PRESENCE || "null", // typing, recording, online, null
-    TIME_ZONE: process.env.TIME_ZONE || "Asia/Karachi", // Enter your timezone
+    TIME_ZONE: process.env.TIME_ZONE || "Asia/Islamabad", // Enter your timezone
 };
 
 // Auto-reload this config when file changes
