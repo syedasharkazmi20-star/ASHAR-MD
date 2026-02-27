b2e4d385895794db77fb6b262deff4f495cfb1ce
syedasharkazmi20-star <syedasharkazmi20@gmail.com>
2026-02-27 03:12:30 -0800

    Update SESSION_ID in config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const fs = require('fs'),
 global.session = "https://stark-md-pair.onrender.com";
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "STARK-MD~hjVKWGwbZ+4wBpX3s4gIePwCUQlDafwcAAA==", // Add sess Id here especially when deploying on panels else use app.json and .env file...
+    SESSION_ID: process.env.SESSION_ID || "Ashar-MD~hjVKWGwbZ+4wBpX3s4gIePwCUQlDafwcAAA==", // Add sess Id here especially when deploying on panels else use app.json and .env file...
     SUDO_NUMBERS: process.env.SUDO_NUMBERS || "", // Add multiple numbers with country codes without (+), separated by comma
     ANTI_DELETE: process.env.ANTI_DELETE || "true", // ✅ only ONE, default = true
     AUTO_STATUS_VIEWS: process.env.AUTO_STATUS_VIEWS || "true",
